8dccde2c264a06597ce16c2f3a44c6d37aff4cf8
yuki4san <yuki.shimura@webempath.com>
2021-05-07 18:26:09 +0900

    test

--- myfunc/main.c
+++ myfunc/main.c
@@ -1,9 +1,17 @@
-#include <stdio.h>
-#include "sum.h"
+/* main.c */
 
+#include <stdio.h>
+#include "sum.h" /* ← sum.h ヘッダーファイルをインクルードする*/
+
+// main関数（メイン関数）
+// main関数（メイン関数）＝さまざまな関数（sum関数含め)の機能をまとめて「全体を管理する機能」が備わっている関数
 int main (void) {
+    // int型変数valueを定義
 	int value;
+    // valueにsum関数の戻り値を代入する
+    // 50と100を引数にとる
 	value = sum(50,100);
+    // ターミナルにvalueに代入された値を表示（150です！）
 	printf("%d\n",value);
 	return 0;
 }
--- myfunc/sum.c
+++ myfunc/sum.c
@@ -1,7 +1,14 @@
 /* sum.c */
+
+// sum関数
+// Int型の引数を2つとり、それらを足す
+
 int sum(int min,int max)
 {
+    // Int型変数numを定義
 	int num;
+    // numに引数の値2つを足した値を代入
 	num = min + max;
+    // numの値を戻り値として返す！
 	return num;
 }
